10dab70afeefb17bf26a22597d3e79f4553488af
Pedro Haupt <pedro.haupt@parceirocantu.com.br>
2025-03-28 11:03:39 -0300

    Estilizando melhor

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 function SetLength(linha,response){
 
-            response = response.concat("</table><br><table><tr><td><label>TAMANHO LINHA:</label></td><td> " + linha.length + "</td></tr></table></div>")
+            response = response.concat("<tr><td> </td><td </td></tr><tr><td><label>TAMANHO LINHA:</label></td><td> " + linha.length + "</td></tr></table></div>")
 
         return response
 }
